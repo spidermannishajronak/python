ad4189e33a7798fd48fc04d500a24e2b76fc2881
spidermannishajronak <rakibnorahouse@gmail.com>
2025-05-15 20:55:41 +0600

    Add files via upload

---
+++ data.py
@@ -0,0 +1,23 @@
+a = "nishaj"
+print (type(a))
+
+b = 25
+print(type(b))
+
+c = 7.3
+print(type(c))
+
+d = True
+print(type(d))
+
+e = [25,7,4,45,48]
+print(type(e))
+
+f = (7,9,20,45)
+print(type(f))
+
+g = {'name' :'nishaj','grade':3}
+print(type(g))
+
+h = {1,2,3,4,5}
+print(type(h))

b42d962830fa85777b025fd9fb50929d38614df2
spidermannishajronak <rakibnorahouse@gmail.com>
2025-06-12 20:48:50 +0600

    Add files via upload

---
+++ 103_nested.py
@@ -0,0 +1,13 @@
+username = input("ENTER YOUR USERNAME:")
+
+if username == "NISHAJ7500":
+    
+    password = input("ENTER YOUR PASSWORD:")
+    
+    if password == "SPIDER-MAN":
+        print("LOGIN SUCCESSFUL!!!") 
+    else:
+        print("INVALID PASSWORD!")
+        
+else:
+    print("INVALID USERNAME!")

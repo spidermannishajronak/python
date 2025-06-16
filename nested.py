5e3bc512d98bfd4986331b9c56afb2397b8c148f
spidermannishajronak <rakibnorahouse@gmail.com>
2025-06-16 23:40:20 +0600

    Add files via upload

---
+++ nested.py
@@ -0,0 +1,16 @@
+name = input("ENTER YOUR NAME:")
+
+if name == "NISHAJ":
+    
+    roll = input("ENTER YOUR ROLL:")
+    
+    if roll == "32":
+        print("LOGIN SUCCESSFUL!!!") 
+    else:
+        print("INVALID ROLL!")
+        
+else:
+    print("INVALID NAME!")
+    
+    
+    

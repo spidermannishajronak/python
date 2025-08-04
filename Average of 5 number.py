b43dab2a07e7350a0e6458ebad968ca32f4d7da7
spidermannishajronak <rakibnorahouse@gmail.com>
2025-08-04 14:50:00 +0600

    Add files via upload

---
+++ Average of 5 number.py
@@ -0,0 +1,12 @@
+a = int(input("enter red ball's number:"))
+b = int(input("enter blue ball's number:"))
+c = int(input("enter yelow ball's number:"))
+d = int(input("enter green ball's number:"))
+e = int(input("enter orange ball's number:"))
+
+total = a+b+c+d+e
+
+
+avg = total/5
+
+print("Result" , avg)

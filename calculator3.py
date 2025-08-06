f03dcf0a2b8e26c5525730f16f72db9c67d73972
spidermannishajronak <rakibnorahouse@gmail.com>
2025-08-06 16:24:36 +0600

    Add files via upload

---
+++ calculator3.py
@@ -0,0 +1,23 @@
+print("addition")
+
+a = int(input("enter 1st number: "))
+b = int(input("enter 2nd number: "))
+print("result:" , a+b)
+
+print("minus")
+
+a = int(input("enter 1st number: "))
+b = int(input("enter 2nd number: "))
+print("result:" , a-b)
+
+print("multiply")
+
+a = int(input("enter 1st number: "))
+b = int(input("enter 2nd number: "))
+print("result:" , a*b)
+
+print("division")
+
+a = int(input("enter 1st number: "))
+b = int(input("enter 2nd number: "))
+print("result:" , a/b)

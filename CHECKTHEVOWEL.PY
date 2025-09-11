ce075f5d9f3791f95bd14853da69d824c50610f1
NISHAJ RONAK <rakibnorahouse@gmail.com>
2025-09-11 16:34:56 +0600

    Add files via upload

---
+++ CHECKTHEVOWEL.PY
@@ -0,0 +1,6 @@
+letter = input("ENTER A LETTER:")
+
+if letter in ["a",  "e" ,"i", "o", "u" , "A", "E" ,"I", "O", "U" ]:
+    print("THIS LETTER IS VOWEL")
+else:
+    print("THIS LETTER IS CONSONENT")

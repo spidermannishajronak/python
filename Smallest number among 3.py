1351f5d7e40b5f18d732b434dd875725f17a3b0e
NISHAJ RONAK <rakibnorahouse@gmail.com>
2025-09-19 14:30:44 +0600

    Add files via upload

---
+++ Smallest number among 3.py
@@ -0,0 +1,13 @@
+a = int(input("ENTER FIRST NUMBER:"))
+b = int(input("ENTER SECOND NUMBER:"))
+c = int(input("ENTER THIRD NUMBER:"))
+
+if a<b and a<c:
+    print("a is the smallest number")
+    
+elif b<a and b<c:
+    print("b is the smallest number")
+    
+else:
+    print("c is the smallest number")
+    

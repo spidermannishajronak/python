989612ba3bdb5dbcab66d37f72089c495d1ce408
NISHAJ RONAK <rakibnorahouse@gmail.com>
2025-10-17 20:40:27 +0600

    Update oddeven at 08 pm friday.py

--- oddeven at 08 pm friday.py
+++ oddeven at 08 pm friday.py
@@ -1,7 +1,8 @@
 n = int(input("ENTER A NUMBER:"))
 
 if n%2 == 0:
-    print("THE NUMBER IS ODD")
+    print("THE NUMBER IS EVEN")
     
 else:
-    print("THE NUMBER IS EVEN")
+
+    print("THE NUMBER IS ODD")

3405f741292d0d1f033868da97bffa6519cc3e47
NISHAJ RONAK <rakibnorahouse@gmail.com>
2025-12-03 22:33:22 +0600

    Add files via upload

---
+++ Larger number among 3.py
@@ -0,0 +1,13 @@
+a = int(input("ENTER 1st NUMBER:"))
+b = int(input("ENTER 2nd NUMBER:"))
+c = int(input("ENTER 3rd NUMBER:")) 
+
+if a>b and a>c:
+    print(a ,"IS THE LARGEST NUMBER")
+    
+    
+elif b>a and b>c:
+    print(b ,"IS THE LARGEST NUMBER")
+    
+else:
+    print(c , "IS THE LARGEST NUMBER")

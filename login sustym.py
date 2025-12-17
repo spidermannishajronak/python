21c564e121fb88d2e1b9e2a7d5f75627cadd4de9
NISHAJ RONAK <rakibnorahouse@gmail.com>
2025-12-18 00:01:13 +0600

    Add files via upload

---
+++ login sustym.py
@@ -0,0 +1,13 @@
+username = input("ENTER YOUR USERNAME:")
+
+if username == "Jeremmyspider":
+    password = input("ENTER YOUR PASSWORD:")
+    
+    if password == "12345678r":
+        print("SUCCESFULLY LOGGED IN")
+        
+    else:
+        print("INVALID USERNAME OR PASSWORD")
+        
+else:
+    print("LOGIN FAILED")

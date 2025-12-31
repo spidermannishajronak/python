2044ae0c47fd5bf62a888fbbecc3bb4d35e51c0e
NISHAJ RONAK <rakibnorahouse@gmail.com>
2025-12-31 23:44:50 +0600

    Add files via upload

---
+++ Diamond Partern.py
@@ -0,0 +1,63 @@
+row = int(input("ENTER THE NUMBER OF ROWS:"))
+
+if row % 2 == 0:
+    half = row // 2
+    
+else:
+    half = row // 2+1
+    
+space = half - 1
+
+for i in range(1, half + 1):
+    x = " " * space
+    y = "*" * (2 * i - 1)
+    print(x+y)
+    
+    space = space - 1
+    
+space = 1
+
+for i in range (half - 1, 0, -1):
+    x = " " * space
+    y = "*" * (2 * i - 1)
+    print(x+y)
+    
+    space = space + 1
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
